056da36f62da85fe3f84b70393cbcd4de36ceee3
reillydonovan <reillydonovan3@gmail.com>
2018-01-10 21:04:53 -0500

    weird bug resolved, fps back to where it should be

--- Assets/Scripts/TogglePingo.cs
+++ Assets/Scripts/TogglePingo.cs
@@ -7,21 +7,21 @@ public class TogglePingo : MonoBehaviour
 
     public GameObject[] togglePingo;
     bool toggle;
-    bool toggle2;
+   // bool toggle2;
 
     void Start()
     {
 
-        toggle = false;
-        toggle2 = true;
+        toggle = true;
+     //   toggle2 = true;
     }
     // Use this for initialization
     public void togglePingoObject()
     {
         toggle = !toggle;
-        toggle2 = !toggle2;
+      //  toggle2 = !toggle2;
         togglePingo[0].SetActive(toggle);
-        togglePingo[1].SetActive(toggle2);
+      //  togglePingo[1].SetActive(toggle2);
 
     }
 }
