820a96bbe4e7b7881183d44926cf45677a3f03a2
reillydonovan <reillydonovan3@gmail.com>
2018-01-15 10:47:31 -0500

    things are looking good, phew

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+    public GameObject ObjectToMove;
+    
+    // Use this for initialization
+
+    public void MoveUp()
+    {
+        Vector3 ObjectPosition = ObjectToMove.transform.position;
+        ObjectPosition.y += 0.1f;
+        ObjectToMove.transform.position = ObjectPosition;
+      //  Debug.Log("moved up" + ObjectPosition.y);
+    }
+    public void MoveDown()
+    {
+        Vector3 ObjectPosition = ObjectToMove.transform.position;
+        ObjectPosition.y -= 0.1f;
+        ObjectToMove.transform.position = ObjectPosition;
+      //  Debug.Log("moved down" + ObjectPosition.y);
+    }
+}
